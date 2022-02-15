cc417c3dbb0329c0d1f488312282e41b8461130c
Ayush Katiyar <89083667+ayushkxyz@users.noreply.github.com>
2022-02-15 10:55:09 +0530

    Update ex-1.cpp

--- Exception Handling/ex-1.cpp
+++ Exception Handling/ex-1.cpp
@@ -1,1 +1,21 @@
 
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    int res=a-b;
+    try{
+        if(a-b!=0){
+            cout<<"The result is :"<<res/a;
+        }
+        else{
+            throw res;
+        }
+    }
+    catch(int r){
+       cout<<"Exception Caught\n";
+       
+    }
+    return 0;
+}
